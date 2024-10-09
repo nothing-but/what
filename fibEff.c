ea0520f8f32898fad6847abbf609ae9d9a59c3b5
zermelo <zermelo@aSUS>
2024-10-09 20:04:35 +0200

    Added 'raw' command line option for fibEff.c

--- fibEff.c
+++ fibEff.c
@@ -7,11 +7,14 @@ long limit = 0;
 
 int main(int argc, char *argv[]) {
 	// Get User Input 
-	if (argc != 2)
+	if (argc < 2 || argc > 3)
 	{
 		printf("Improper input. Exiting.\n");
 		return -1; 
 	}
+	
+	//"raw" command line option
+	_Bool raw = argc == 3 && argv[2][0] == '-' && argv[2][1] == 'r';
 	
 	limit = strtol(argv[1], NULL, 10);
 	
@@ -23,13 +26,20 @@ int main(int argc, char *argv[]) {
 	mpz_fib_ui(res, limit);
 	
 	clock_t end_time = clock();
+
+	//"raw" command line option
+	if(!raw) {
+	  printf("Fibonacci Number %ld: ", limit);
+	}
 	
-	printf("Fibonacci Number %ld: ", limit);
    	mpz_out_str(stdout, 10, res);
    	printf("\n");
 
    	mpz_clear(res);
 
-    double time_taken = ((double) end_time - start_time)  / CLOCKS_PER_SEC;
-    printf("Calculation Time: %f seconds\n", time_taken);
+	double time_taken = ((double) end_time - start_time)  / CLOCKS_PER_SEC;
+        
+	if(!raw) {
+	  printf("Calculation Time: %f seconds\n", time_taken);
+	}
 }
